82f6d0860c877e59c6a6183d59134d8470002552
reginaldo <reginaldo.rc.castro@gmail.com>
2025-12-25 10:32:47 -0400

    Remocao das url dos tokes para url do app

--- backend/os_servico/urls.py
+++ backend/os_servico/urls.py
@@ -1,9 +1,18 @@
 from rest_framework.routers import DefaultRouter
 from .views import OrdemServicoViewSet, OrdemServicoChecklistViewSet, ChecklistItemViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'ordens-servico', OrdemServicoViewSet, basename='ordem-servico')
 router.register(r'ordens-servico-checklist', OrdemServicoChecklistViewSet, basename='ordens-servico-checklist')
 router.register(r'checklist', ChecklistItemViewSet, basename='checklist')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls))
+]
