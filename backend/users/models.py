6134b0b8f64362bfc335673d5db6cd60a3b8924f
reginaldo <reginaldo.rc.castro@gmail.com>
2025-12-25 21:36:53 -0400

    Adicionado o perfil de usuario

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    class Perfil(models.TextChoices):
+        ADMIN = 'admin', 'Administrador'
+        TECNICO = 'tecnico', 'Técnico'
+    
+    perfil = models.CharField(max_length=10, choices=Perfil.choices, default=Perfil.TECNICO)
