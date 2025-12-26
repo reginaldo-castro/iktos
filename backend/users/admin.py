2c0c6968a344388d828692a8f4f38ad343cb726c
reginaldo <reginaldo.rc.castro@gmail.com>
2025-12-25 21:36:33 -0400

    Inclusao no painel admin do usuario

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+class MyUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('perfil',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('perfil',)}),
+    )
+
+admin.site.register(User, MyUserAdmin)
